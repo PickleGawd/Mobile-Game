dc350c3faf074b2f0db20c7b1690b1767a0e78d4
TheGawdSquad <44072151+TheGawdSquad@users.noreply.github.com>
2018-11-29 13:16:14 -0800

    School Commit

---
+++ Dodge The Blocks/Assets/_Scripts/TimerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+
+	public GameObject timer;
+
+	private bool startTimer = true;
+
+	private int countDown = 10;
+	
+	// Update is called once per frame
+	void Update () {
+
+		gameObject.GetComponent<Text>().text = "Time Remaining: " + countDown;
+
+		if (startTimer)
+		{
+			StartCoroutine(StartTime());
+			startTimer = false;
+		}
+
+		if (countDown <= 0)
+		{
+			//PlayerPrefs.SetInt("Level", PlayerPrefs.GetInt("Level") - 1);
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+
+	IEnumerator StartTime()
+	{
+		yield return new WaitForSeconds(1f);
+
+		startTimer = true;
+		countDown--;
+	}
+}
